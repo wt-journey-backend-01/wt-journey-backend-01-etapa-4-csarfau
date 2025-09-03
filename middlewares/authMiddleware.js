ec0d27cde3e4e44fd6d01e3c0d30824a19581964
Cesar <csarfau@gmail.com>
2025-09-03 10:35:20 -0300

    feat: add verify token in blacklist

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,15 +1,21 @@
 import { createError } from '../utils/errorHandler.js';
 import jwt from 'jsonwebtoken';
+import { blacklist } from '../blacklist';
 
 export function authMiddleware(req, res, next) {
   const authHeader = req.headers['authorization'];
   const token = authHeader?.split(' ')[1];
+  const secret = process.env.JWT_SECRET || 'segredo';
 
   if (!token) {
     return next(createError(401, 'Token inválido!'));
   }
 
-  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+  if(blacklist.has(token)) {
+    return next(createError(401, 'Token inválido!'));
+  }
+
+  jwt.verify(token, secret, (err, user) => {
     if (err) {
       return next(createError(401, 'Token inválido!'));
     }
