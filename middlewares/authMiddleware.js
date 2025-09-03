d7494d227b5e25c9e310a933b254bf09a66b530d
Cesar <csarfau@gmail.com>
2025-09-03 00:26:28 -0300

    feat: auth middleware to verify jwt token

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,19 @@
+import { createError } from '../utils/errorHandler.js';
+import jwt from 'jsonwebtoken';
+
+export function authMiddleware(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader?.split(' ')[1];
+
+  if (!token) {
+    return next(createError(401, 'Token inválido!'));
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      return next(createError(401, 'Token inválido!'));
+    }
+    req.user = user;
+    next();
+  });
+}
